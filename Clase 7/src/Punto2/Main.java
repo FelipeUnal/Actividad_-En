f98f6689e833a1d24d980f24452cf3a60571bb4d
Estudiante <Estudiante@168.176.37.107>
2017-02-27 09:00:41 -0500

    pago de factura

---
+++ Clase 7/src/Punto2/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Punto2;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Main {
+    
+    public static void main(){
+        Commission c = new Commission();
+        c.computeCommission(20,10);
+        System.out.println(c);
+    }
+    
+}
